f4f411732b645ee0e2dce490c14cdf6e2ae69d80
ShiSheffer55 <shirasheffer839@gmail.com>
2024-12-24 00:53:52 -0800

    added massage's constructors

--- Message.java
+++ Message.java
@@ -3,10 +3,35 @@ package test;
 import java.util.Date;
 
 public class Message {
+
     public final byte[] data;
     public final String asText;
     public final double asDouble;
     public final Date date;
 
+    public Message(byte[] data) {
+        this.data = data;
+        this.asText = data.toString();
+        this.asDouble = tryParse(this.asText);
+        this.date = new Date();
+    }
 
+
+    public Message(String asText) {
+       this(asText.getBytes());
+    }
+
+
+    public Message(Double asDouble) {
+        this(asDouble.toString());
+    }
+
+    private double tryParse(String data){
+        try{
+            return Double.parseDouble(data);
+        }catch(NumberFormatException e){
+                System.out.println(e.getMessage());
+                return Double.NaN;
+        }
+    }
 }
